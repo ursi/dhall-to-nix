dc546c3941947cc4e48b64fa12bbf97bde240c86
Mason Mackaman <masondeanm@aol.com>
2020-12-20 23:23:28 -0500

    Remove unnecessary code

--- flake.nix
+++ flake.nix
@@ -4,7 +4,7 @@
       let
         der = stdenv.mkDerivation {
           name = "dhall-to-nix";
-          dhall = builtins.toFile "dhall" (builtins.readFile path);
+          dhall = path;
           dontUnpack = true;
           buildInputs = [ dhall-json ];
           installPhase = "dhall-to-json --file $dhall --output $out";
